ad9a451489cf83be7e65edbd852069f71aaa4ab6
Anis Mekacher <haithem1297@outlook.fr>
2021-08-25 14:41:29 +0000

    installing latest version of docker compose if it doesn't exist.

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ check_packages() {
         sh get-docker.sh
         rm get-docker.sh
     fi
-    if ! command -v docker-compose &>/dev/null; then
+    if [ ! -f /usr/local/bin/docker-compose ]; then
         echo "installing docker-compose..."
         curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         chmod +x /usr/local/bin/docker-compose
