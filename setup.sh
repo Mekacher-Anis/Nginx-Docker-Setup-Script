bd0552f5deed3fe5189807acc35a508f0ebb56ef
Mekacher-Anis <anismkdev@gmail.com>
2022-03-25 17:03:37 +0100

    Improved log while updating.

--- setup.sh
+++ setup.sh
@@ -322,8 +322,9 @@ update() {
         read -n 1 ans
         if [ $ans = 'y' ]; then
             # make a backup of the old configuration
-            echo -e "\nA backup of the old configuration has been made at /home/$USERNAME/nginx-backup.tar.gz.\n You can restore this version by running this script with '--restore' command"
+            echo -e "\n[LOG] Creating backup of the old configuration..."
             tar -C $NGINX_ROOT_FOLDER -czf /home/$USERNAME/nginx-backup.tar.gz .
+            echo -e "A backup of the old configuration has been made at /home/$USERNAME/nginx-backup.tar.gz.\nYou can restore this version by running this script with '--restore' command"
 
             # build the new images
             create_images 1
